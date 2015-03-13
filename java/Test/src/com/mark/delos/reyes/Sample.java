0b57498d6e6ec5abb0ecc022095000a9a3077697
Mark <oj.delosreyes@gmail.com>
2015-03-13 16:02:54 +0900

    Initial commit

---
+++ java/Test/src/com/mark/delos/reyes/Sample.java
@@ -0,0 +1,12 @@
+package com.mark.delos.reyes;
+
+public class Sample {
+	protected String lname;
+	protected String fname;
+	protected String mname;
+	
+	public static int main(String[] args) {
+		System.out.println("Hello World!");
+		return 0;
+	}
+}
